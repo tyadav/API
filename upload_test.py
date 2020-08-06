5aa0dff0826bcd0b577f578efd28f4c178ef60a6
Tej <tej.narayan@gmail.com>
2020-08-06 20:08:09 +0530

    Add files via upload

---
+++ upload_test.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  6 13:35:15 2020
+
+@author: TejYadav
+"""
+import flask
+from flask import request
+#from werkzeug.utils import secure_filename
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route("/me")
+def me_api():
+    user = get_current_user()
+    return {
+        "username": user.username,
+        "theme": user.theme,
+        "image": url_for("user_image", filename=user.image),
+    }
+
+@app.route("/users")
+def users_api():
+    users = get_all_users()
+    return jsonify([user.to_json() for user in users])
+
+'''
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['the_file']
+        f.save('/var/www/uploads/uploaded_file.txt')
+'''       
+        
+app.run(host='0.0.0.0')
